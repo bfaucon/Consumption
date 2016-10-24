022cd4945ee3b5bb79eac8a367e70d6dcd8583ea
Bruno Faucon <bruno.faucon@one2care.be>
2016-10-24 14:46:13 +0200

    Security Update
    Removing username/pwd

--- thermo.py
+++ thermo.py
@@ -44,8 +44,8 @@ import MySQLdb   # MySQLdb must be installed by yourself
 PATH_THERM = "/home/pi/consumption/" #path to this script
 PATH_LOG = "/home/pi/consumption/log" #path to this script
 DB_SERVER ='localhost'  # MySQL : IP server (localhost if mySQL is on the same machine)
-DB_USER='root'     # MySQL : user
-DB_PWD='Kate0130'            # MySQL : password
+DB_USER='*User*'     # MySQL : user
+DB_PWD='********'            # MySQL : password
 DB_BASE='consumption'     # MySQL : database name
  
 # vous pouvez ajouter ou retirer des sondes en modifiant les 5 lignes ci dessous
